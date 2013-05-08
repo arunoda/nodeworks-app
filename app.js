dc6eb4017f9b01cb74ddbb0e5787a749f13c1b9f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-05-09 05:25:37 +0530

    added first version

---
+++ app.js
@@ -0,0 +1,12 @@
+var express     = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+
+    res.send("Welcome to Node-EB");
+});
+
+var port = process.env.PORT || 3000;
+console.log('starting app on port:', port);
+
+app.listen(port);
